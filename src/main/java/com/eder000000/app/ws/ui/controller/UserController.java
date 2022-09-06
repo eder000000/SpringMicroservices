02e2d3ea8a1eea6cf1ee797be5d9814b015dab2d
edera <edera@amdocs.com>
2022-09-06 00:03:32 -0500

    Returning Object as JSON or XML Representation

--- src/main/java/com/eder000000/app/ws/ui/controller/UserController.java
+++ src/main/java/com/eder000000/app/ws/ui/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.eder000000.app.ws.ui.controller;
 
 import model.response.UserRest;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
 
 @RestController
 @RequestMapping("users") //http://localhost:8080/users
@@ -13,7 +16,9 @@ public class UserController {
                            @RequestParam(value ="sort", defaultValue = "desc", required = false) String sort){
         return "get user was called with page=" + page + " and limits= " + limit + "and sort = " + sort;
     }
-    @GetMapping(path = "/{userId}")
+    @GetMapping(path = "/{userId}", produces = {
+                                    MediaType.APPLICATION_XML_VALUE,
+                                    MediaType.APPLICATION_JSON_VALUE})
     public UserRest getUser(@PathVariable String userId){
         UserRest returnValue = new UserRest();
         returnValue.setEmail("test@test.com");
