20ef93150e6c9326993ece90e90a096155e8d5b9
edera <edera@amdocs.com>
2022-09-05 23:05:05 -0500

    Adding PathVariable_Annotation

--- src/main/java/com/eder000000/app/ws/ui/controller/UserController.java
+++ src/main/java/com/eder000000/app/ws/ui/controller/UserController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("users") //http://localhost:8080/users
 public class UserController {
 
-    @GetMapping
-    public String getUser(){
-        return "get user was called";
+    @GetMapping(path = "/{userId}")
+    public String getUser(@PathVariable String userId){
+        return "get user was called with " + userId;
     }
 
     @PostMapping
